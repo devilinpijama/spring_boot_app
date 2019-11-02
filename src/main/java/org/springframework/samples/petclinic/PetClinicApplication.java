0fe39d39de68ea22663ccd650eac21ab0efb1ca6
Rakivnenko Dmytro <faros111999@gmail.com>
2019-11-03 01:35:23 +0200

    1

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -26,10 +26,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class PetClinicApplication  {
+public class PetClinicApplication {
 
-   public static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(PetClinicApplication.class, args);
-   }
+    }
 
 }
