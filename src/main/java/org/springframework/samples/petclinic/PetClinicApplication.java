6deda30b3e7b15f2ea2330417d84eb7f0060da8f
Rakivnenko Dmytro <faros111999@gmail.com>
2019-11-03 02:32:40 +0200

    Update PetClinicApplication.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -16,20 +16,22 @@
 
 package org.springframework.samples.petclinic;
 
-import org.springframework.boot.builder.SpringApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-//- public class PetClinicApplication {
-//
-  //  public static void main(String[] args) {
-    //    SpringApplication.run(PetClinicApplication.class, args);
-    //}
-
-//}
-public class PetClinicApplication extends SpringBootServletInitializer {
-
+@SpringBootApplication
+public class PetClinicApplication extends SpringBootServletInitializer{
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(PetClinicApplication.class);
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder app) { 
+      return app.sources(PetClinicApplication.class); 
     }
-}
+    
+    public static void main(String[] args){
+    SpringApplication.run(PetClinicApplication.class, args);
+     }
+     }
+
+
+
